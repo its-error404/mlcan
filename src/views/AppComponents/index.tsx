ac24ad993b2d52574ec2b4c55f0ebd4819d5bdf7
Nishanth <nishanth.shanmuganathan@rootquotient.com>
2023-01-05 11:53:55 +0530

    General: Updating boilerplate as required by package
    Added dropdown, datepicker, switch, radio, otp components

--- src/views/AppComponents/index.tsx
+++ src/views/AppComponents/index.tsx
@@ -1,5 +1,4 @@
-import { Button, Radio, Select } from "antd";
-import Checkbox from "antd/lib/checkbox/Checkbox";
+import { Button, Col, Row } from "antd";
 import { Form, Formik } from "formik";
 import React, { FC } from "react";
 import InputField from "../../shared/components/InputField";
@@ -11,14 +10,14 @@ const options = [
   { label: "Orange", value: "Orange" },
 ];
 
-interface AppComponentsProps {}
+interface AppComponentsProps { }
 
 const AppComponents: FC<AppComponentsProps> = (props) => {
-  const {} = props;
+  const { } = props;
   return (
     <div className="app-components">
       <h1 className="text-center text-decoration">App Components</h1>
-      <Formik initialValues={{}} onSubmit={() => {}}>
+      <Formik initialValues={{}} onSubmit={() => { }}>
         <Form>
           <InputField type="text" name="input" placeholder="Enter some text" />
           <div className="mt-5">
@@ -26,29 +25,6 @@ const AppComponents: FC<AppComponentsProps> = (props) => {
               Primary Button
             </Button>
             <Button>Default Button</Button>
-          </div>
-          <div className="mt-5">
-            <Checkbox /> Checkbox
-          </div>
-          <div className="mt-5">
-            <Radio.Group defaultValue={1}>
-              <Radio value={1}>A</Radio>
-              <Radio value={2}>B</Radio>
-              <Radio value={3}>C</Radio>
-              <Radio value={4}>D</Radio>
-            </Radio.Group>
-          </div>
-
-          <div className="mt-5">
-            <Radio.Group
-              options={options}
-              defaultValue={"Apple"}
-              optionType="button"
-              buttonStyle="solid"
-            />
-          </div>
-          <div className="mt-5">
-              <Select options={options}/>
           </div>
         </Form>
       </Formik>
