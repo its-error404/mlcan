73afa95335041afca3885c52ffcb81cf5c52e5d5
karthikeyan_AK <karthikeyan.athipalam@rootquotient.com>
2023-09-14 01:44:52 +0530

    changed models

--- src/models/repairForm.model.ts
+++ src/models/repairForm.model.ts
@@ -35,7 +35,7 @@ export class NonMaerskDetails {
     @serializable(alias('desc', primitive()))
     desc?: string
 
-    @serializable(alias('ID Source', primitive()))
+    @serializable(alias('id', primitive()))
     id?: string
 }
 
